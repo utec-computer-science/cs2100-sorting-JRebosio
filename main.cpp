bc1691803f0ec2a7b0ec2cedacbf0e53caaf6c06
Jorge Rebosio <jorge.rebosio@utec.edu.pe>
2020-04-07 21:55:12 -0500

    fix(main.cpp)

--- main.cpp
+++ main.cpp
@@ -1,20 +1,47 @@
 #include "common_types.h"
 #include "Sort.h"
+ 
 
 
 int main(){
-    vector <int> vec;
-    vec.push_back(40);
-    vec.push_back(80);
-    vec.push_back(16);
-    vec.push_back(30);
-    Sort<HeapSort> HS;
-    Sort<MergeSort> MS;
-    Sort<QuickSort> QS;
-    Sort<InsertionSort> IS;
-    Sort<SelectionSort> SS;
-    Sort<BubbleSort> BS;
-    //BS(vec);
+  auto shell_it (ostream_iterator<int>{cout, ", "});
+ const size_t data_points{100};
+ const int mean {57};
+ const size_t dev {3};
+ 
+
+  random_device rd;
+  mt19937 gen {rd()};
+  normal_distribution<  > d {mean, dev};
+
+
+  vector<int> uno,dos,tres,cuatro,cinco,seis;
+  uno.reserve(data_points);
+  generate_n(back_inserter(uno), data_points, [&] { return d(gen); });
+
+
+  copy(uno.begin(), uno.end(), back_inserter(dos)); 
+  copy(uno.begin(), uno.end(), back_inserter(tres)); 
+  copy(uno.begin(), uno.end(), back_inserter(cuatro)); 
+  copy(uno.begin(), uno.end(), back_inserter(cinco)); 
+  copy(uno.begin(), uno.end(), back_inserter(seis)); 
+    
+
+    Sort<InsertionSort> IS(uno);
+    Sort<HeapSort> HS(dos);
+    Sort<MergeSort> MS(tres);
+    Sort<QuickSort> QS(cuatro);
+    Sort<SelectionSort> SS(cinco);
+    Sort<BubbleSort> BS(seis);
+    
+
+
+    cout<<"\nInsertion Sort :\n\n";copy(uno.begin(),uno.end(),shell_it); 
+    cout<<"\n\nHeap Sort :\n\n";copy(dos.begin(),dos.end(),shell_it);
+    cout<<"\n\nMerge Sort :\n\n";copy(tres.begin(),tres.end(),shell_it); 
+    cout<<"\n\nQuick Sort :\n\n";copy(cuatro.begin(),cuatro.end(),shell_it); 
+    cout<<"\n\nSelection Sort :\n\n";copy(cinco.begin(),cinco.end(),shell_it); 
+    cout<<"\n\nBubble Sort :\n\n";copy(seis.begin(),seis.end(),shell_it); 
     
     return 0;
 }
